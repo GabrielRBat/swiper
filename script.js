593c674ec971e845aef31522485f69f03ab64e1e
GabrielRBat <144289224+GabrielRBat@users.noreply.github.com>
2025-05-16 15:58:27 +0000

    allow touch to move remove

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const propertySlider = new Swiper(".swiper.p-images", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
   slidesPerView: 1,
   spaceBetween: 16,
+  allowTouchMove: false, 
 
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-btn-next",
     prevEl: ".swiper-btn-prev",
